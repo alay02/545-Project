e4464aa54ee1379fd05d20e6e56d990621184712
alay02 <alaypatel02@gmail.com>
2023-12-05 22:36:41 -0500

    545 project Initial

---
+++ script.js
@@ -0,0 +1,7 @@
+ // Add your JavaScript code here
+function performSearch() {
+    // Add code here to handle the search functionality
+    var searchTerm = document.getElementById('search-bar').value;
+    // Perform search based on the searchTerm
+    console.log('Searching for: ', searchTerm);
+}
